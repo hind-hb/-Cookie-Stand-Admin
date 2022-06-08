664388aca53d04deb09b68197a4e6445bf77581f
hind-hb <hind.hbashenh@gmail.com>
2022-06-09 00:04:35 +0300

    get value

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,32 @@
-import Header  from "./Components/Header"
-import Main from "./Components/main"
-import Footer from "./Components/Footer"
+import Header from "./Components/Header";
+import Main from "./Components/main";
+import Footer from "./Components/Footer";
+
 import { useState } from "react";
-function Home (){
-  const [report, setReport] = useState('Report Tabel Comming Soon ...');
 
- 
-return(
-  <>
-  <title>Cookie Stand Admin</title>
-  <Header/>
-  <Main/>
-  
-  <Footer/>
-  
-  </>
-)
+export default function Home() {
+  const [report, setReport] = useState((""));
 
+  function formHandle(event) {
+    event.preventDefault();
+    const Location = {
+      location: event.target.location.value,
+      Min: event.target.min.value,
+      Max: event.target.max.value,
+      Avg: event.target.avg.value,
+    };
+
+    setReport(Location);
+  }
+  return (
+    <div >
+      
+      <Header />
+      <Main formHandle={formHandle} />
+      <h1 className="text-center text-black">Report Table coming soon.............</h1>
+      <h1 className="text-center text-black">{JSON.stringify(report)}</h1>
+
+      <Footer />
+    </div>
+  );
 }
-export default Home 
